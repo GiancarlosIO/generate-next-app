ffdbe27ba6593062ae83a4a145a6a7b8f446d15e
Giancarlos Isasi <giancarlos.isasi@gmail.com>
2021-09-11 18:33:16 -0500

    feat: add query client providers in app.txs

--- src/templates/_app.ts
+++ src/templates/_app.ts
@@ -1,9 +1,21 @@
 export default `
-import '../../styles/globals.css';
+import * as React from 'react';
 import type { AppProps } from 'next/app';
 
+import { QueryClient, QueryClientProvider } from 'react-query';
+import { Hydrate } from 'react-query/hydration';
+
+import '../../styles/globals.css';
+
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  const [queryClient] = React.useState(() => new QueryClient());
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Hydrate>
+        <Component {...pageProps} />
+      </Hydrate>
+    </QueryClientProvider>
+  );
 }
 export default MyApp;
 
